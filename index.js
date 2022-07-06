7191808cd52acc1f5af96fbbf31f12640cf38db3
Aarya135 <56118531+Aarya135@users.noreply.github.com>
2022-07-06 06:13:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+switch(randomNumber1){
+  case 1: document.querySelectorAll("img")[0].setAttribute("src","images/dice1.png");
+    break;
+  case 2: document.querySelectorAll("img")[0].setAttribute("src","images/dice2.png");
+  break;
+  case 3: document.querySelectorAll("img")[0].setAttribute("src","images/dice3.png");
+  break;
+  case 4: document.querySelectorAll("img")[0].setAttribute("src","images/dice4.png");
+  break;
+  case 5: document.querySelectorAll("img")[0].setAttribute("src","images/dice5.png");
+  break;
+  case 6: document.querySelectorAll("img")[0].setAttribute("src","images/dice6.png");
+  break;
+  default:document.querySelectorAll("img")[0].setAttribute("src","images/dice6.png");
+}
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+switch(randomNumber2){
+  case 1: document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
+    break;
+  case 2: document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
+  break;
+  case 3: document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png");
+  break;
+  case 4: document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
+  break;
+  case 5: document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
+  break;
+  case 6: document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+  break;
+  default:document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+}
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "🎊Player 1 wins!";
+}
+else if (randomNumber1==randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Player 2 wins!🎊 ";
+}
